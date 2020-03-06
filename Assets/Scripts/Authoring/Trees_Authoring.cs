5b33fcc727ca2a3f0ca75a2808dea29340136964
Victor <victorr@unity3d.com>
2020-03-06 10:29:14 -0500

    WIP: Trees spawner (Dynamic buffer)
    WIP: It was developed a set of scripts for instance Trees using a Dynamic buffer.

---
+++ Assets/Scripts/Authoring/Trees_Authoring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Trees_Authoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject[] TreePrefabs;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        DynamicBuffer<TreeBuffer> dynamicBuffer = dstManager.AddBuffer<TreeBuffer>(entity);
+
+        for (int i = 0; i < TreePrefabs.Length; i++)
+        {
+            var prefab = conversionSystem.GetPrimaryEntity(TreePrefabs[i]);
+            dynamicBuffer.Add(new TreeBuffer() {Prefab = prefab});
+        }
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        for (int i = 0; i < TreePrefabs.Length; i++)
+        {
+            referencedPrefabs.Add( TreePrefabs[i] );
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/TreesSpawnerSystem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Collections;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class TreesSpawnerSystem : JobComponentSystem
+{
+    private EntityQuery m_entityQuery;
+    
+    protected override void OnCreate()
+    {
+        m_entityQuery = GetEntityQuery(typeof(TreesSpawner));
+    }
+    
+    [BurstCompile]
+    struct PlaceTreesJob : IJobParallelFor
+    {
+        [ReadOnly]
+        [DeallocateOnJobCompletion]
+        public NativeArray<Entity>                  Instances;
+
+        [NativeDisableParallelForRestriction]
+        public ComponentDataFromEntity<Translation> Translations;
+
+        public void Execute(int index)
+        {
+            for (int i = 0; i < Instances.Length; i++)
+            {
+                Entity instanceEntity   = Instances[i];
+                float3 position         = new float3(8.25f, 0, i * 2.5f);
+
+                Translations[instanceEntity] = new Translation { Value = position };
+            }
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.WithStructuralChanges().ForEach((Entity entity, in TreesSpawner treeSpawner, in LocalToWorld location) =>
+        {
+            inputDeps.Complete();
+            var treeBuffer = EntityManager.GetBuffer<TreeBuffer>(entity);
+
+            int roadLength = 10;
+            int treesPerRoadFragment = 3;
+            
+            /*NativeArray<Entity> instances = new NativeArray<Entity>( roadLength * treesPerRoadFragment, Allocator.TempJob, NativeArrayOptions.ClearMemory);
+            EntityManager.Instantiate(treeBuffer[0].Prefab, instances);
+            
+            inputDeps = new PlaceTreesJob
+            {
+                Instances = instances,
+                Translations = GetComponentDataFromEntity<Translation>()
+            }.Schedule( roadLength * treesPerRoadFragment, 1, default);
+            */
+            EntityManager.DestroyEntity(entity);
+        }).Run();
+
+        return inputDeps;
+    }
+}
